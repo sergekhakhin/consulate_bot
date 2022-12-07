990bb0a77e31808e9ba1219ba21b88a1c8e4524b
Sergey Khakhin <sergekhakhin@gmail.com>
2022-12-07 18:57:46 +0400

    +

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 from time import sleep
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.webdriver import Service as ChromeService
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 from captcha_solver import solve_captcha
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-url = ""
+service = ChromeService(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+url = "https://gyumri.kdmid.ru/queue/orderinfo.aspx?id=16487&cd=00b29879&ems=BF464C54"
 
 
 def main() -> None:
     driver.get(url)
+    sleep(2)
     fill_captcha_textbox(solve_captcha(get_captcha_img()))
-    sleep(3)
+    sleep(5)
     submit_captcha()
-    sleep(3)
+    sleep(5)
     click_enroll_button()
-    sleep(15)
+    sleep(5)
 
 
-def get_captcha_img() -> str:
-    return driver.find_element(By.ID, "ctl00_MainContent_imgSecNum").screenshot_as_base64
+def get_captcha_img() -> bytes:
+    return driver.find_element(By.ID, "ctl00_MainContent_imgSecNum").screenshot_as_png
+
+
+def save_captcha_img(img: bytes) -> None:
+    with open("captcha.png", "wb") as f:
+        f.write(img)
 
 
 def fill_captcha_textbox(solution: str) -> None:
@@ -40,5 +48,5 @@ def click_enroll_button() -> None:
     enroll_button.click()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
